3aa77b9550cbd78578edfb23bacff6719e2fafe2
Steve Thomson <organicpanda@gmail.com>
2015-09-04 20:10:24 +0100

    Replace mmp with mmap.js

--- test/hgt-spec.js
+++ test/hgt-spec.js
@@ -21,7 +21,7 @@ test('can query hgt nearestneighbour', function(t) {
     var hgt = new Hgt(__dirname + '/data/N57E011.hgt', [57, 11], {
         interpolation: Hgt.nearestNeighbour
     });
-    
+
     t.equal(hgt.getElevation([57, 11]), 0);
     t.equal(hgt.getElevation([57.7, 11.9]), 16);
     hgt.destroy();
@@ -32,7 +32,7 @@ test('can query hgt bilinear', function(t) {
     var hgt = new Hgt(__dirname + '/data/N57E011.hgt', [57, 11], {
         interpolation: Hgt.bilinear
     });
-    
+
     t.equal(hgt.getElevation([57, 11]), 0);
     almostEqual(t, hgt.getElevation([57.7, 11.9]), 16);
     hgt.destroy();

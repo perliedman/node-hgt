d3dd4e7168960c2bc16854fa3d90136370b6fd7b
Richard Jansen <richard.jansen@lajos.nl>
2016-01-05 13:13:32 +0100

    added test for downloading, unzipping and querying non-existing tiles

--- test/tileset-spec.js
+++ test/tileset-spec.js
@@ -1,4 +1,7 @@
 var test = require('tape'),
+    os = require('os'),
+    fs = require('fs'),
+    rimraf = require('rimraf'),
     almostEqual = function(t, actual, expected, delta, msg) {
         var d = Math.abs(actual - expected);
         delta = delta || 1e-9;
@@ -30,7 +33,7 @@ test('can query tileset', function(t) {
     });
 });
 
-test('can\'t query non-existing tiles', function(t) {
+test('can\'t query non-existing tiles without downloader', function(t) {
     var tileset = new TileSet(__dirname + '/data/', {downloader:null});
     tileset.getElevation([52.7, 11.9], function(err, elevation) {
         if (!err) {
@@ -40,5 +43,27 @@ test('can\'t query non-existing tiles', function(t) {
         }
         tileset.destroy();
         t.end();
+    });
+});
+
+test('can query non-existing tiles with default downloader', function(t) {
+    var tmpDir = os.tmpDir() + '/data' + Date.now();
+    fs.mkdir(tmpDir, function(error) {
+        if (error) {
+            t.fail(error);
+            return;
+        }
+
+        var tileset = new TileSet(tmpDir);
+        tileset.getElevation([52.7, 11.9], function(err, elevation) {
+            if (!err) {
+                t.ok(true, 'getElevation for non-existing tile returned: ' + elevation);
+            } else {
+                t.fail('getElevation gave an error for non-existing tile: ' + err);
+            }
+            tileset.destroy();
+            t.end();
+            rimraf(tmpDir, function(){});
+        });
     });
 });
